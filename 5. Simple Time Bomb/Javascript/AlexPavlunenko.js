9f7b07f183b6cfea0b85da986efbc69396a4e351
PM-Code-Wars <alex.pavlunenko@peoplematter.com>
2016-08-22 14:41:03 -0400

    Create AlexPavlunenko.js

---
+++ 5. Simple Time Bomb/Javascript/AlexPavlunenko.js
@@ -0,0 +1,6 @@
+//console.log (CutTheWire.toString());
+//console.log (global[theWire].toString());
+//console.log (this[theWire].toString());
+
+var wire = global[theWire]; // Find the wire.
+CutTheWire(wire);
