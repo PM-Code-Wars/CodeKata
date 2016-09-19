2e8f5af42a4ed163cdce0ff5100ff2c97fd3ad0d
PM-Code-Wars <alex.pavlunenko@peoplematter.com>
2016-09-19 10:32:28 -0400

    Update EvanMcGann.rb

--- 7. Moves in squared strings/Ruby/EvanMcGann.rb
+++ 7. Moves in squared strings/Ruby/EvanMcGann.rb
@@ -1,11 +1,11 @@
 # Moves in squared strings (I):
 def vert_mirror(s)
-    s.split("\n").each { |x| x.reverse! }.join("\n")
+    s.split("\n").map(&:reverse).join("\n")
 end
 def hor_mirror(s)
-    subs = s.split("\n").reverse.join("\n")
+    s.split("\n").reverse.join("\n")
 end
-def oper(fct, s)
+def oper(fct, s) 
     fct.call(s)
 end
 
@@ -22,33 +22,20 @@ def oper(fct, s)
 end
 
 # Moves in squared strings (III)
-def rot_90_clock(s)
-    diag_1_sym(s).split("\n").map { |line| line.reverse }.join("\n")
-end
 def diag_1_sym(s)
-    subs = s.split("\n")
-    new_subs = []
-    for i in 0 ... subs.length
-        line = subs[i]
-        new_line = ""
-        for j in 0 ... line.length
-            new_line += subs[j][i]
-        end
-        new_subs.push(new_line)
-    end
-    new_subs.join("\n")
+    transpose(s).join("\n")
+end
+def rot_90_clock(s)
+    transpose(s).map(&:reverse).join("\n")
 end
 def selfie_and_diag1(s)
-    subs = s.split("\n")
-    diag = diag_1_sym(s).split("\n")
-    res = []
-    for i in 0 ... diag.length
-      res.push(subs[i] + "|" + diag[i])
-    end
-    res.join("\n")    
+    s.split("\n").zip(diag_1_sym(s).split("\n")).map{|lines| lines.join("|")}.join("\n")
 end
 def oper(fct, s) 
     fct.call(s)
+end
+def transpose(s)
+    s.split("\n").map(&:chars).transpose.map(&:join)
 end
 
 # Moves in squared strings (IV)
@@ -62,11 +49,7 @@ def selfie_diag2_counterclock(s)
     selfie = s.split("\n")
     diag = diag_2_sym(s).split("\n")
     rot = rot_90_counter(s).split("\n")
-    res = []
-    for i in 0 ... selfie.length
-      res.push(selfie[i] + "|" + diag[i] + "|" + rot[i])
-    end
-    res.join("\n")
+    selfie.zip(diag, rot).map{|lines| lines.join("|")}.join("\n")
 end
 def oper(fct, s) 
     fct.call(s)
@@ -74,3 +57,4 @@ end
 def transpose(s)
   s.split("\n").map(&:chars).transpose.map(&:join)
 end
+
